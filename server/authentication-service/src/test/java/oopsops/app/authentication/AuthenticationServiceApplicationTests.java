74d329b8af313adc57eaca9f31bda45c9a16ce15
Hamza <chaoukihamza16@gmail.com>
2025-07-13 22:38:55 +0200

    UPDATE: Added tests for authentication service

--- server/authentication-service/src/test/java/oopsops/app/authentication/AuthenticationServiceApplicationTests.java
+++ server/authentication-service/src/test/java/oopsops/app/authentication/AuthenticationServiceApplicationTests.java
@@ -1,15 +1,23 @@
 package oopsops.app.authentication;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class AuthenticationServiceApplicationTests {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Test
     void contextLoads() {
+        assertNotNull(applicationContext);
     }
 
 }
