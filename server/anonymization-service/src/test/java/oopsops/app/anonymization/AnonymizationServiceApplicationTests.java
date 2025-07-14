58c6def41aa5ab9978276946d8caf0fbab558a12
Hamza <chaoukihamza16@gmail.com>
2025-07-14 13:17:26 +0200

    UPDATE: Fix Integration test for Anonymization Service

--- server/anonymization-service/src/test/java/oopsops/app/anonymization/AnonymizationServiceApplicationTests.java
+++ server/anonymization-service/src/test/java/oopsops/app/anonymization/AnonymizationServiceApplicationTests.java
@@ -3,8 +3,11 @@ package oopsops.app.anonymization;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+
 import oopsops.app.anonymization.controller.AnonymizationController;
 import oopsops.app.anonymization.repository.AnonymizationRepository;
 import oopsops.app.anonymization.service.AnonymizationService;
@@ -17,6 +20,9 @@ public class AnonymizationServiceApplicationTests {
 
     @Autowired
     private ApplicationContext applicationContext;
+
+    @MockitoBean
+    private JwtDecoder jwtDecoder;
 
     @Test
     void contextLoads() {
